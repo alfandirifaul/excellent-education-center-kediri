bd4963a5f55d51829a768e68faf336ce6bd5231e
alfandi rifaul <125331290+alfandirifaul@users.noreply.github.com>
2025-03-07 22:22:54 +0700

    feat: update routes to include landing page, contact, price, and about views

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,21 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('landing-page.index');
+})->name('home');
+
+Route::get('/contact', function () {
+    return view('landing-page.contact');
+})->name('contact');
+
+Route::get('/price', function () {
+    return view('landing-page.price');
+})->name('price');
+
+Route::get('/about', function () {
+    return view('landing-page.about');
+})->name('about');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +31,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
