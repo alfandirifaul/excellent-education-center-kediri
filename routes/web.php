7a52378c5b2d00b097b039cd83afcd1baa19ff52
alfandi rifaul <125331290+alfandirifaul@users.noreply.github.com>
2025-03-07 22:37:51 +0700

    feat: refactor routes to group landing page views and add about page

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,24 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('landing-page.index');
-})->name('home');
+Route::prefix('')->group(function () {
+    Route::get('/', function () {
+        return view('landing-page.index');
+    })->name('home');
 
-Route::get('/contact', function () {
-    return view('landing-page.contact');
-})->name('contact');
+    Route::get('/contact', function () {
+        return view('landing-page.contact');
+    })->name('contact');
 
-Route::get('/price', function () {
-    return view('landing-page.price');
-})->name('price');
+    Route::get('/price', function () {
+        return view('landing-page.price');
+    })->name('price');
 
-Route::get('/about', function () {
-    return view('landing-page.about');
-})->name('about');
+    Route::get('/about', function () {
+        return view('landing-page.about');
+    })->name('about');
+});
+
 
 
 Route::get('/dashboard', function () {
